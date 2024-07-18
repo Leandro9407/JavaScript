12cd505a0260be9d9b86288fcd5ca06a936f4c27
Yeison Leandro Arroyave <fullm94@gmail.com>
2024-07-18 11:01:01 -0500

    Agrego ejercicio 5 casi terminado

---
+++ Ejercicio5/js/codigo.js
@@ -0,0 +1,54 @@
+
+let producto = [];
+let cantidad = [];
+
+function agregarProducto(){
+    
+    const mostrando = document.getElementById("mostrando_producto");
+    let agregando_p = document.getElementById("productoAgregado").value;
+    let agregando_c = parseInt (document.getElementById("cantidadAgregado").value);
+
+    producto.push(agregando_p);
+    cantidad.push(agregando_c);
+
+    let añadir_inventario = "";
+    producto.forEach(function(producto, indice){
+    añadir_inventario += '<li class="list-group-item list-group-item-danger d-flex justify-content-between align-items-center">' + producto;
+    añadir_inventario += '<span class="badge text-bg-primary rounded-pill">' + cantidad[indice] + '</span></li>';
+    });
+    mostrando.innerHTML = añadir_inventario;
+
+}
+
+
+
+function modificarProducto(){
+
+    const actualizando = document.getElementById("mostrando_producto");
+    let modificar_p = document.getElementById("productoComparar").value;
+    let modificar_c = parseInt (document.getElementById("cantidadModificado").value);
+    let producto_actualizado = document.getElementById("productoModificado").value;
+
+    for (let i=0; i<producto.length; i++){
+         if (modificar_p==producto[i]){
+             producto[i] = producto_actualizado;
+             cantidad[i] = modificar_c;
+        }     
+            
+    }
+
+    let añadir_inventario = "";
+    producto.forEach(function(producto, indice){
+    añadir_inventario += '<li class="list-group-item list-group-item-danger d-flex justify-content-between align-items-center">' + producto;
+    añadir_inventario += '<span class="badge text-bg-primary rounded-pill">' + cantidad[indice] + '</span></li>';
+    });
+
+    actualizando.innerHTML = añadir_inventario;
+
+
+    function eliminar(){
+     
+        
+    }
+
+}
